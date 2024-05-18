4497d1309b2ab92eba4d86cc3ff9feedd0255ada
amazondelivery <48974775+amazondelivery@users.noreply.github.com>
2024-05-18 16:56:47 -0700

    neat wrapRect() function!

--- main.py
+++ main.py
@@ -12,23 +12,31 @@ screen_height = 500
 screen = pg.display.set_mode((screen_width, screen_height))
 clock = pg.time.Clock()
 
+def wrapRect(obj, color='Black'):
+    rect = obj.get_rect()
+    pg.draw.rect(obj, color, rect, 1)
+    return obj
+
 def introSequence():
     font = introFont
     minifont = miniIntroFont
     screen.fill("White")
-    rockText = "hello! i'm rock, and i need YOUR help to defeat my nemisis, paper!"
     title = font.render("Press play to play Rock Paper!", True, "Black")
-    playButton = font.render('Play', True, 'Black')
+    playButton = wrapRect(font.render('Play', True, 'Black'))
+    settingsButton = font.render("Settings", True, 'Black')
     audio = minifont.render("audio: ", True, 'Black')
-    rockScaleFactor = 0.45
 
     screen.blit(title, (50,50))
     screen.blit(playButton, (200, 170))
-    screen.blit(audio, (5, 0))
+    screen.blit(audio, (5, 0)) #audio text
+    screen.blit(settingsButton, (200, 200))
 
+def introStory():
+    rockScaleFactor = 0.45
+    rockText = "hello! i'm rock, and i need YOUR help to defeat my nemisis, paper!"
     screen.blit(pg.transform.smoothscale(rock,
                 (Xrock * rockScaleFactor, Yrock * rockScaleFactor)),
-                (0, 320))
+                (0, 320)) #rock image
 
 while True:
     for event in pg.event.get():
